21d77040a3acc79b051735e5168d4c0308a4c8b7
Mohamed Mostafa <ibrahim.mohamed@gizasystems.com>
2024-08-18 17:50:34 +0300

    connect with an ngrok backend

--- src/frontend/index.jsx
+++ src/frontend/index.jsx
@@ -17,7 +17,20 @@ const App = () => {
     const data = await res.json();
     return [data.fields.summary, data.fields.description.content[0].content[0].text];
   };
-
+  fetch('https://34bd-105-88-48-92.ngrok-free.app')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();  // or response.text() if the response is not JSON
+    })
+    .then(data => {
+      console.log('Response data:', data);
+      // Handle the data here
+    })
+    .catch(error => {
+      console.error('There was a problem with the fetch operation:', error);
+    });
   const fetchAllIssues = async () => {
     const res = await requestJira('/rest/api/3/search?');
     const data = await res.json();
