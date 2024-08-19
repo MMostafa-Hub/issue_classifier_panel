ab6f7a35d149f6cdfff7727a35d0415c434d41fd
Mohamed Mostafa <moh.mostafa.ibra@gmail.com>
2024-08-19 18:49:56 +0300

    feat: Add API integration for creating collections, getting predictions, and adding issues

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,17 +1,56 @@
 import Resolver from '@forge/resolver';
-import api, { route } from "@forge/api";
-const resolver = new Resolver();
+import axios from 'axios';
 
-resolver.define('getText', (req) => {
-  console.log(req);
-  const response = api.asUser().requestJira(route`/rest/api/3/search`, {
-    headers: {
-      'Accept': 'application/json',
-      'Authorization': 'Bearer'
+const resolver = new Resolver();
+export const BASE_URL = `https://chief-unified-moccasin.ngrok-free.app`;
+
+const API = axios.create({ baseURL: BASE_URL });
+
+resolver.define('createCollection', async (req) => {
+  const { collection_name } = req.payload;
+  try {
+    const res = await API.post(`/collection`, null, {
+      params: {
+        collection_name: collection_name
+      }
     }
-  });
-  console.log(response)
-  return "response";
+    );
+    return res.data;
+  } catch (err) {
+    throw err;
+  }
+});
+
+resolver.define('getPrediction', async (req) => {
+  const { summary, description } = req.payload;
+  try {
+    const res = await API.get(`/prediction`, {
+      params: {
+        summary: summary,
+        description: description
+      }
+    }
+    );
+    return res.data;
+  } catch (err) {
+    throw err;
+  }
+});
+
+resolver.define('addIssues', async (req) => {
+  const { issues } = req.payload;
+
+  try {
+    const res = await API.post(`/issues`, issues, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+    );
+    return res.data;
+  } catch (err) {
+    throw err;
+  }
 });
 
 export const handler = resolver.getDefinitions();
